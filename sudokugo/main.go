3f6a7df7279f4dca1a25cf55872218d965c2f1a2
kombinat <kovacs213janos@gmail.com>
2024-03-05 15:50:09 +0100

    kiindulasi tabla, kiiratas fuggveny, random szam bekeres

---
+++ sudokugo/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Sudoku [Size][Size]int
+
+type Cella struct {
+	Sor, Oszlop int
+}
+
+const Size = 9
+
+func kiiratasTabla(tabla Sudoku) {
+	for i := 0; i < Size; i++ {
+		for j := 0; j < Size; j++ {
+			fmt.Printf("%d ", tabla[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func szambekeres() int {
+	var szam int
+	for {
+		fmt.Print("Írj ide egy számot 1 és 89 között ")
+		_, err := fmt.Scanln(&szam)
+		if err != nil {
+			fmt.Println("Invalid szám. Normálisan!")
+			continue
+		}
+
+		if szam < 1 || szam > 89 {
+			fmt.Println("A kapott szám nem 1 és 89 között van!")
+			continue
+		}
+
+		break
+	}
+	return szam
+}
+
+func main() {
+	Tabla_Eredeti := [Size][Size]int{
+		{5, 3, 4, 6, 7, 8, 9, 1, 2},
+		{6, 7, 2, 1, 9, 5, 3, 4, 8},
+		{1, 9, 8, 3, 4, 2, 5, 7, 6},
+		{8, 5, 9, 7, 6, 1, 4, 2, 3},
+		{4, 2, 6, 8, 5, 3, 7, 9, 1},
+		{7, 1, 3, 9, 2, 4, 8, 5, 6},
+		{9, 6, 1, 5, 3, 7, 2, 8, 4},
+		{2, 8, 7, 4, 1, 9, 6, 3, 5},
+		{3, 4, 5, 2, 8, 6, 1, 7, 9},
+	}
+
+	fmt.Println("Ez a kiindulási tábla:")
+	kiiratasTabla(Tabla_Eredeti)
+	fmt.Println("")
+	randomSzam := szambekeres()
+	fmt.Println("A kapott szám:", randomSzam)
+	fmt.Println("Ennyit fogunk véletlenszerűen kivenni az eredeti táblából.")
+}
